06a7e92b5f8eab2e6047b41e86669d8d8e245c6d
Oscar SeungJun Park <seungjun.park025@gmail.com>
2025-03-30 19:10:43 +0900

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+class DB:
+    def __init__(self):
+        self._db = {}
+
+    def put(self, key: str, value: any):
+        self._db[key] = value
+
+    def get(self, key: str) -> any | None:
+        return self._db.get(key)
+
+    def exists(self, key: str) -> bool:
+        keys = self._db.keys()
+        for k in keys:
+            if k == key:
+                return True
+        return False
