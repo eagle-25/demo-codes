722d1b83bab231ae06c49ff5393d68dc4ba70fd7
Oscar SeungJun Park <seungjun.park025@gmail.com>
2025-03-30 19:14:39 +0900

    feat: DB 클래스 수정

--- demo.py
+++ demo.py
@@ -6,11 +6,13 @@ class DB:
         self._db[key] = value
 
     def get(self, key: str) -> any | None:
-        return self._db.get(key)
+        if key not in self._db.keys():
+            raise KeyError(f"Key {key} not found")
+        else:
+            return self._db[key]
 
     def exists(self, key: str) -> bool:
-        keys = self._db.keys()
-        for k in keys:
-            if k == key:
-                return True
-        return False
+        return key in self._db
+
+    def delete(self, key: str):
+        self._db.pop(key)
